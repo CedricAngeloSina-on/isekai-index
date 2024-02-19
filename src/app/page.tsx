e355adf7a96fb5f206d9f4416d37243f578cd524
CedricAngeloSina-on <137179865+CedricAngeloSina-on@users.noreply.github.com>
2024-02-19 13:43:23 +0800

    chore: tested fetch and jikan api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
-export default function Home() {
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+
+export default async function Home() {
+    let data = await fetch(
+        "https://api.jikan.moe/v4/anime?genres=24&page=1&limit=1"
+    );
+    let json = await data.json();
+
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 text-xl font-black">
-            Isekai Index
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Card>
+                <CardHeader>
+                    <CardTitle>Isekai Index</CardTitle>
+                    <CardDescription>
+                        The Database for Isekai Degens
+                    </CardDescription>
+                </CardHeader>
+            </Card>
+            <p>{JSON.stringify(json)}</p>
         </main>
     );
 }
