e0797b3a46e405f9bb92b568756bbd807aed1501
CedricAngeloSina-on <cao.sinaon@gmail.com>
2024-02-26 21:45:33 +0800

    chore: update next.config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import { getSeasonNowAllData } from "@/utils/getSeasonNowAllData";
+import AnimeCard from "@/components/ui/anime-card";
+export default async function Home() {
+    const data = await getSeasonNowAllData();
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24 text-xl font-black">
             Isekai Index
+            {data
+                .filter((entry: any) =>
+                    entry.themes.some(
+                        (theme: { mal_id: number }) => theme.mal_id === 62
+                    )
+                )
+                .map((entry, index) => (
+                    <AnimeCard
+                        cover_image={entry.images.jpg.image_url}
+                        cover_image_alt={entry.title + " Cover image"}
+                        title={entry.title}
+                        studio_name={entry.studios[0].name}
+                        synopsis={entry.synopsis}
+                    ></AnimeCard>
+                ))}
         </main>
     );
 }
