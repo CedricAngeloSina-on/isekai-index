a456d9f581fa94182e69df573848f125f8b84bb9
CedricAngeloSina-on <137179865+CedricAngeloSina-on@users.noreply.github.com>
2024-03-01 00:52:00 +0800

    chore: remove card header placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,10 @@
 import { Suspense } from "react";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card";
 import AnimeSeasonList from "@/components/anime-season-list";
 import AnimeSeasonListSkeleton from "@/components/skeleton/anime-season-list-skeleton";
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between bg-slate-900">
-            <Card className="my-10">
-                <CardHeader>
-                    <CardTitle>Isekai&apos;Dex</CardTitle>
-                </CardHeader>
-            </Card>
             <Suspense fallback={<AnimeSeasonListSkeleton />}>
                 <AnimeSeasonList />
             </Suspense>
