4c85c8aa60a7779dcc974ab9175ef41cc5081fea
CedricAngeloSina-on <137179865+CedricAngeloSina-on@users.noreply.github.com>
2024-03-03 17:47:19 +0800

    feat: added custom util getSeasonAllData
    fetches specific anime season that accepts params: year and season name

---
+++ src/utils/getSeasonAllData.tsx
@@ -0,0 +1,41 @@
+import { Anime } from "@/types/jikanAnime.types";
+import { JikanResponse } from "@/types/jikanResponse.types";
+
+import filterIsekaiAnime from "./filterIsekaiAnime";
+
+type SeasonParams = {
+    year: number;
+    season: string;
+};
+
+export async function getSeasonAllData({
+    year,
+    season,
+}: SeasonParams): Promise<Anime[]> {
+    const allData: Anime[] = [];
+    let page = 1;
+
+    while (true) {
+        const response = await fetch(
+            `https://api.jikan.moe/v4/seasons/${year}/${season}?page=${page}`
+        );
+
+        const data = (await response.json()) as JikanResponse<Anime[]>;
+
+        await new Promise((resolve) => setTimeout(resolve, 400));
+
+        if (
+            !data.pagination ||
+            (data.pagination.has_next_page === false &&
+                data.pagination.current_page !==
+                    data.pagination.last_visible_page)
+        ) {
+            break;
+        }
+
+        allData.push(...filterIsekaiAnime(data.data));
+
+        page++;
+    }
+    return allData;
+}
