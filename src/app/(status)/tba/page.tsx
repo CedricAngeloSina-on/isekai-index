5bb2859797aa5f5c03e11c7f7537b8a9410ded03
CedricAngeloSina-on <137179865+CedricAngeloSina-on@users.noreply.github.com>
2024-03-13 00:37:20 +0800

    feat: tba page

--- src/app/(status)/tba/page.tsx
+++ src/app/(status)/tba/page.tsx
@@ -1,14 +1,14 @@
 import { Suspense } from "react";
-import AnimeSeasonList from "@/components/anime-season-list";
+import AnimeSeasonListUpcoming from "@/components/anime-season-list-upcoming";
 import AnimeSeasonListSkeleton from "@/components/skeleton/anime-season-list-skeleton";
 
 export default function TBA() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between bg-slate-900">
-            ARCHIVE
-            {/* <Suspense fallback={<AnimeSeasonListSkeleton />}>
-                <AnimeSeasonList />
-            </Suspense> */}
+            TBA
+            <Suspense fallback={<AnimeSeasonListSkeleton />}>
+                <AnimeSeasonListUpcoming />
+            </Suspense>
         </main>
     );
 }
