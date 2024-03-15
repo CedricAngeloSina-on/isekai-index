7f24abdb0c9159ccd871580323fd8b4fa4f08b4a
CedricAngeloSina-on <137179865+CedricAngeloSina-on@users.noreply.github.com>
2024-03-16 06:37:40 +0800

    style: remove text

--- src/app/(status)/tba/page.tsx
+++ src/app/(status)/tba/page.tsx
@@ -3,7 +3,6 @@ import AnimeSeasonListUpcoming from "@/components/anime-season-list-upcoming";
 export default function TBA() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between bg-slate-900">
-            TBA
             <AnimeSeasonListUpcoming />
         </main>
     );
