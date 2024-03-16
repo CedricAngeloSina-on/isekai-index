ba6829b43351a7f5a73b34109c1f4e20dde4b365
CedricAngeloSina-on <137179865+CedricAngeloSina-on@users.noreply.github.com>
2024-03-16 18:01:22 +0800

    style: bg-color

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,7 +4,7 @@ import Menu from "@/components/navbar/menu";
 
 export default function Navbar() {
     return (
-        <div className="flex flex-col items-center justify-between bg-gray-700 px-0">
+        <div className="flex flex-col items-center justify-between bg-slate-900/95 px-0">
             <div className="container mx-0 grid h-56 w-full grid-cols-1 gap-4 px-3 pt-10 sm:grid-cols-2 md:grid-cols-3">
                 <Logo />
                 <SeasonLinks />
