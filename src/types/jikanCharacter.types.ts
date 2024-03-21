d010afa3f0cc0881eb80dd8d1df6a22b99d05750
CedricAngeloSina-on <cao.sinaon@gmail.com>
2024-03-21 22:23:22 +0800

    refactor: character types

--- src/types/jikanCharacter.types.ts
+++ src/types/jikanCharacter.types.ts
@@ -1,19 +1,35 @@
 export type Character = {
-    mal_id: number;
-    url: string;
-    images: {
-        jpg: {
-            image_url: string;
-            small_image_url: string;
+    character: {
+        mal_id: number;
+        url: string;
+        images: {
+            jpg: {
+                image_url: string;
+                small_image_url: string;
+            };
+            webp: {
+                image_url: string;
+                small_image_url: string;
+            };
         };
-        webp: {
-            image_url: string;
-            small_image_url: string;
-        };
+        name: string;
     };
-    name: string;
     name_kanji: string;
     nicknames: string[];
     favorites: number;
     about: string;
+    role: string;
+    voice_actors: {
+        person: {
+            mal_id: number;
+            url: string;
+            images: {
+                jpg: {
+                    image_url: string;
+                };
+            };
+            name: string;
+        };
+        language: string;
+    }[];
 };
