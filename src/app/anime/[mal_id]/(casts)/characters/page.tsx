9c88e9b359161cc7658818d0810b75a70b3912b5
CedricAngeloSina-on <cao.sinaon@gmail.com>
2024-03-21 22:26:03 +0800

    feat: implemented anime character list to pages

--- src/app/anime/[mal_id]/(casts)/characters/page.tsx
+++ src/app/anime/[mal_id]/(casts)/characters/page.tsx
@@ -1,3 +1,22 @@
-export default function Characters() {
-    return <div className="text-white">CHARACTERS</div>;
+import { getAnimeCharacters } from "@/utils/getAnimeCharacters";
+import CharacterCard from "@/components/character-card";
+
+export default async function Characters({
+    params: { mal_id },
+}: {
+    params: { mal_id: number };
+}) {
+    const characterData = await getAnimeCharacters(mal_id);
+    return (
+        <div className="flex h-auto w-full flex-col space-y-4 px-8 pb-8 text-left text-slate-400">
+            <div className="flex text-base font-semibold capitalize leading-tight tracking-wider text-slate-400">
+                Characters
+            </div>
+            <div className="grid grid-cols-2 gap-x-8 gap-y-3">
+                {characterData.map((character, index) => (
+                    <CharacterCard key={index} {...character} />
+                ))}
+            </div>
+        </div>
+    );
 }
--- src/app/anime/[mal_id]/(casts)/overview/page.tsx
+++ src/app/anime/[mal_id]/(casts)/overview/page.tsx
@@ -1,3 +1,82 @@
-export default function Overview() {
-    return <div className="text-white">Overview</div>;
+import { getAnimeCharacters } from "@/utils/getAnimeCharacters";
+import CharacterCard from "@/components/character-card";
+
+export default async function Overview({
+    params: { mal_id },
+}: {
+    params: { mal_id: number };
+}) {
+    const characterData = await getAnimeCharacters(mal_id);
+
+    return characterData ? (
+        <div className="flex h-auto w-full flex-col space-y-4 px-8 pb-8 text-left text-slate-400">
+            <div className="flex text-base font-semibold capitalize leading-tight tracking-wider text-slate-400">
+                Characters
+            </div>
+            <div className="grid grid-cols-2 gap-x-8 gap-y-3">
+                {characterData.slice(0, 9).map((character, index) => (
+                    <CharacterCard key={index} {...character} />
+                ))}
+            </div>
+            <div className="flex text-base font-semibold capitalize leading-tight tracking-wider text-slate-400">
+                Staff
+            </div>
+            <div className="grid grid-cols-2 gap-x-6 gap-y-3">
+                <div className="flex h-32 w-full overflow-hidden rounded-sm bg-red-500">
+                    <div className="flex">
+                        <div className="h-full w-24 bg-sky-500"></div>
+                        <div className="flex flex-col justify-between p-1 text-left">
+                            <p>STAFF</p>
+                            <p>POSITION</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex h-32 w-full overflow-hidden rounded-sm bg-red-500">
+                    <div className="flex">
+                        <div className="h-full w-24 bg-sky-500"></div>
+                        <div className="flex flex-col justify-between p-1 text-left">
+                            <p>STAFF</p>
+                            <p>POSITION</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex h-32 w-full overflow-hidden rounded-sm bg-red-500">
+                    <div className="flex">
+                        <div className="h-full w-24 bg-sky-500"></div>
+                        <div className="flex flex-col justify-between p-1 text-left">
+                            <p>STAFF</p>
+                            <p>POSITION</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex h-32 w-full overflow-hidden rounded-sm bg-red-500">
+                    <div className="flex">
+                        <div className="h-full w-24 bg-sky-500"></div>
+                        <div className="flex flex-col justify-between p-1 text-left">
+                            <p>STAFF</p>
+                            <p>POSITION</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex h-32 w-full overflow-hidden rounded-sm bg-red-500">
+                    <div className="flex">
+                        <div className="h-full w-24 bg-sky-500"></div>
+                        <div className="flex flex-col justify-between p-1 text-left">
+                            <p>STAFF</p>
+                            <p>POSITION</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex h-32 w-full overflow-hidden rounded-sm bg-red-500">
+                    <div className="flex">
+                        <div className="h-full w-24 bg-sky-500"></div>
+                        <div className="flex flex-col justify-between p-1 text-left">
+                            <p>STAFF</p>
+                            <p>POSITION</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    ) : null;
 }
