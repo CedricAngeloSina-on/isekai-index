6bd323235fb2f32de631147770f0d592345de1d5
CedricAngeloSina-on <137179865+CedricAngeloSina-on@users.noreply.github.com>
2024-03-22 10:54:41 +0800

    feat: created staff page

--- src/app/anime/[mal_id]/(casts)/staff/page.tsx
+++ src/app/anime/[mal_id]/(casts)/staff/page.tsx
@@ -1,3 +1,23 @@
-export default function Staff() {
-    return <div className="text-white">STAFF</div>;
+import { getAnimeStaff } from "@/utils/getAnimeStaff";
+import StaffCard from "@/components/staff-card";
+
+export default async function Staff({
+    params: { mal_id },
+}: {
+    params: { mal_id: number };
+}) {
+    const staffData = await getAnimeStaff(mal_id);
+
+    return (
+        <div className="flex h-auto w-full flex-col space-y-4 px-8 pb-8 text-left text-slate-400">
+            <div className="flex text-base font-semibold capitalize leading-tight tracking-wider text-slate-400">
+                Staff
+            </div>
+            <div className="grid grid-cols-2 gap-x-8 gap-y-3">
+                {staffData.map((staff, index) => (
+                    <StaffCard key={index} {...staff} />
+                ))}
+            </div>
+        </div>
+    );
 }
