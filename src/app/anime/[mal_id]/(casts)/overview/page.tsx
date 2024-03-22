7cf49f35f3638d6500290a33d056673db8110492
CedricAngeloSina-on <137179865+CedricAngeloSina-on@users.noreply.github.com>
2024-03-22 10:53:51 +0800

    feat: added staff to overview page

--- src/app/anime/[mal_id]/(casts)/overview/page.tsx
+++ src/app/anime/[mal_id]/(casts)/overview/page.tsx
@@ -1,5 +1,7 @@
 import { getAnimeCharacters } from "@/utils/getAnimeCharacters";
+import { getAnimeStaff } from "@/utils/getAnimeStaff";
 import CharacterCard from "@/components/character-card";
+import StaffCard from "@/components/staff-card";
 
 export default async function Overview({
     params: { mal_id },
@@ -7,6 +9,7 @@ export default async function Overview({
     params: { mal_id: number };
 }) {
     const characterData = await getAnimeCharacters(mal_id);
+    const staffData = await getAnimeStaff(mal_id);
 
     return characterData ? (
         <div className="flex h-auto w-full flex-col space-y-4 px-8 pb-8 text-left text-slate-400">
@@ -14,68 +17,17 @@ export default async function Overview({
                 Characters
             </div>
             <div className="grid grid-cols-2 gap-x-8 gap-y-3">
-                {characterData.slice(0, 9).map((character, index) => (
+                {characterData.slice(0, 10).map((character, index) => (
                     <CharacterCard key={index} {...character} />
                 ))}
             </div>
-            <div className="flex text-base font-semibold capitalize leading-tight tracking-wider text-slate-400">
+            <div className="flex pt-6 text-base font-semibold capitalize leading-tight tracking-wider text-slate-400">
                 Staff
             </div>
             <div className="grid grid-cols-2 gap-x-6 gap-y-3">
-                <div className="flex h-32 w-full overflow-hidden rounded-sm bg-red-500">
-                    <div className="flex">
-                        <div className="h-full w-24 bg-sky-500"></div>
-                        <div className="flex flex-col justify-between p-1 text-left">
-                            <p>STAFF</p>
-                            <p>POSITION</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex h-32 w-full overflow-hidden rounded-sm bg-red-500">
-                    <div className="flex">
-                        <div className="h-full w-24 bg-sky-500"></div>
-                        <div className="flex flex-col justify-between p-1 text-left">
-                            <p>STAFF</p>
-                            <p>POSITION</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex h-32 w-full overflow-hidden rounded-sm bg-red-500">
-                    <div className="flex">
-                        <div className="h-full w-24 bg-sky-500"></div>
-                        <div className="flex flex-col justify-between p-1 text-left">
-                            <p>STAFF</p>
-                            <p>POSITION</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex h-32 w-full overflow-hidden rounded-sm bg-red-500">
-                    <div className="flex">
-                        <div className="h-full w-24 bg-sky-500"></div>
-                        <div className="flex flex-col justify-between p-1 text-left">
-                            <p>STAFF</p>
-                            <p>POSITION</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex h-32 w-full overflow-hidden rounded-sm bg-red-500">
-                    <div className="flex">
-                        <div className="h-full w-24 bg-sky-500"></div>
-                        <div className="flex flex-col justify-between p-1 text-left">
-                            <p>STAFF</p>
-                            <p>POSITION</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex h-32 w-full overflow-hidden rounded-sm bg-red-500">
-                    <div className="flex">
-                        <div className="h-full w-24 bg-sky-500"></div>
-                        <div className="flex flex-col justify-between p-1 text-left">
-                            <p>STAFF</p>
-                            <p>POSITION</p>
-                        </div>
-                    </div>
-                </div>
+                {staffData.slice(0, 4).map((staff, index) => (
+                    <StaffCard key={index} {...staff} />
+                ))}
             </div>
         </div>
     ) : null;
