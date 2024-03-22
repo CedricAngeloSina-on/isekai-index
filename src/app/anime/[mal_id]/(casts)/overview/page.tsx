e78c1b12ddcd532ae5bf9d72c8e6e90d3fe6a522
CedricAngeloSina-on <137179865+CedricAngeloSina-on@users.noreply.github.com>
2024-03-22 11:23:32 +0800

    style: number of cards shown in overview

--- src/app/anime/[mal_id]/(casts)/overview/page.tsx
+++ src/app/anime/[mal_id]/(casts)/overview/page.tsx
@@ -2,6 +2,7 @@ import { getAnimeCharacters } from "@/utils/getAnimeCharacters";
 import { getAnimeStaff } from "@/utils/getAnimeStaff";
 import CharacterCard from "@/components/character-card";
 import StaffCard from "@/components/staff-card";
+import Link from "next/link";
 
 export default async function Overview({
     params: { mal_id },
@@ -13,16 +14,32 @@ export default async function Overview({
 
     return characterData ? (
         <div className="flex h-auto w-full flex-col space-y-4 px-8 pb-8 text-left text-slate-400">
-            <div className="flex text-base font-semibold capitalize leading-tight tracking-wider text-slate-400">
-                Characters
+            <div className="flex justify-between text-base font-semibold leading-tight tracking-wider text-slate-400">
+                <p>Characters</p>
+                <Link
+                    href={"/anime/" + mal_id + "/characters"}
+                    className="flex flex-col items-center hover:text-slate-300"
+                >
+                    <p className="flex justify-center text-base font-medium leading-tight tracking-wider">
+                        More characters
+                    </p>
+                </Link>
             </div>
             <div className="grid grid-cols-2 gap-x-8 gap-y-3">
-                {characterData.slice(0, 10).map((character, index) => (
+                {characterData.slice(0, 6).map((character, index) => (
                     <CharacterCard key={index} {...character} />
                 ))}
             </div>
-            <div className="flex pt-6 text-base font-semibold capitalize leading-tight tracking-wider text-slate-400">
-                Staff
+            <div className="flex justify-between pt-6 text-base font-semibold leading-tight tracking-wider text-slate-400">
+                <p>Staff</p>
+                <Link
+                    href={"/anime/" + mal_id + "/staff"}
+                    className="flex flex-col items-center hover:text-slate-300"
+                >
+                    <p className="flex justify-center text-base font-medium leading-tight tracking-wider">
+                        More staff
+                    </p>
+                </Link>
             </div>
             <div className="grid grid-cols-2 gap-x-6 gap-y-3">
                 {staffData.slice(0, 4).map((staff, index) => (
